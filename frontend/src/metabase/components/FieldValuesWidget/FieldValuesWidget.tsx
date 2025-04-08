63756eb2495a3171c48b8236d96c89eef1a0dc37
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-04-08 15:21:55 -0400

    Fix field size

--- frontend/src/metabase/components/FieldValuesWidget/FieldValuesWidget.tsx
+++ frontend/src/metabase/components/FieldValuesWidget/FieldValuesWidget.tsx
@@ -170,7 +170,6 @@ export const FieldValuesWidgetInner = forwardRef<
 
   const fetchValues = async (query?: string) => {
     setLoadingState("LOADING");
-    setOptions([]);
 
     let newOptions: FieldValue[] = [];
     let newValuesMode = valuesMode;
